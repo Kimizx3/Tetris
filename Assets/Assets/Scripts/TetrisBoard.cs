840a402092193248f00583e128623740898495c4
Kimizx3 <55560064+Kimizx3@users.noreply.github.com>
2024-12-13 16:17:11 -0800

    finished

--- Assets/Assets/Scripts/TetrisBoard.cs
+++ Assets/Assets/Scripts/TetrisBoard.cs
@@ -2,18 +2,32 @@ using UnityEngine;
 using UnityEngine.Tilemaps;
 
 
+[DefaultExecutionOrder(-1)]
 public class TetrisBoard : MonoBehaviour
 {
     public Tilemap tilemap { get; private set; }
+    public Piece activePiece { get; private set; }
     public TetrominoData[] tetrominos;
+    public Vector3Int spawnPosition = new Vector3Int(-1, 8, 0);
+    public Vector2Int boardSize = new Vector2Int(10, 20);
+
+    public RectInt Bounds
+    {
+        get
+        {
+            Vector2Int position = new Vector2Int(-boardSize.x/2,-boardSize.y/2);
+            return new RectInt(position, boardSize);
+        }
+    }
 
     private void Awake()
     {
         this.tilemap = GetComponentInChildren<Tilemap>();
+        this.activePiece = GetComponentInChildren<Piece>();
         
-        for (int i = 0; i < this.tetrominos.Length; i++)
+        for (int i = 0; i < tetrominos.Length; i++)
         {
-            this.tetrominos[i].Initialize();
+            tetrominos[i].Initialize();
         }
     }
 
@@ -22,14 +36,125 @@ public class TetrisBoard : MonoBehaviour
         SpawnPiece();
     }
 
-    private void SpawnPiece()
+    public void SpawnPiece()
     {
-        int random = Random.Range(0, this.tetrominos.Length);
-        TetrominoData data = this.tetrominos[random];
+        int random = Random.Range(0, tetrominos.Length);
+        TetrominoData data = tetrominos[random];
+        
+        activePiece.Initialize(this, spawnPosition, data);
+        if (IsValidPosition(activePiece, spawnPosition))
+        {
+            Set(activePiece);
+        }
+        else
+        {
+            GameOver();
+        }
     }
 
-    public void Set()
+    private void GameOver()
     {
+        tilemap.ClearAllTiles();
         
+        // ...
+    }
+
+    public void Set(Piece piece)
+    {
+        for (int i = 0; i < piece.cells.Length; i++)
+        {
+            Vector3Int tilePosition = piece.cells[i] + piece.position;
+            tilemap.SetTile(tilePosition, piece.data.tiles);
+        }
+    }
+    
+    public void Clear(Piece piece)
+    {
+        for (int i = 0; i < piece.cells.Length; i++)
+        {
+            Vector3Int tilePosition = piece.cells[i] + piece.position;
+            tilemap.SetTile(tilePosition, null);
+        }
+    }
+
+    public bool IsValidPosition(Piece piece, Vector3Int position)
+    {
+        RectInt bounds = Bounds;
+        
+        for(int i = 0; i < piece.cells.Length; i++)
+        {
+            Vector3Int tilePosition = piece.cells[i] + position;
+
+            if (!bounds.Contains((Vector2Int)tilePosition))
+            {
+                return false;
+            }
+
+            if (tilemap.HasTile(tilePosition))
+            {
+                return false;
+            }
+        }
+
+        return true;
+    }
+
+    public void ClearLines()
+    {
+        RectInt bounds = this.Bounds;
+        int row = bounds.yMin;
+
+        while (row < bounds.yMax)
+        {
+            if (IsLineFull(row))
+            {
+                LineClear(row);
+            }
+            else
+            {
+                row++;
+            }
+        }
+    }
+
+    public bool IsLineFull(int row)
+    {
+        RectInt bounds = Bounds;
+
+        for (int col = bounds.xMin; col < bounds.xMax; col++)
+        {
+            Vector3Int position = new Vector3Int(col, row, 0);
+
+            if (!tilemap.HasTile(position))
+            {
+                return false;
+            }
+        }
+        return true;
+    }
+
+    public void LineClear(int row)
+    {
+        RectInt bounds = Bounds;
+
+        for (int col = bounds.xMin; col < bounds.xMax; col++)
+        {
+            Vector3Int position = new Vector3Int(col, row, 0);
+            this.tilemap.SetTile(position,null);
+        }
+
+        while (row < bounds.yMax)
+        {
+            for (int col = bounds.xMin; col < bounds.xMax; col++)
+            {
+                Vector3Int position = new Vector3Int(col, row + 1, 0);
+                TileBase above = tilemap.GetTile(position);
+
+                position = new Vector3Int(col, row, 0);
+                tilemap.SetTile(position, above);
+            }
+
+            row++;
+        }
     }
 }
--- Assets/Assets/Scripts/Tetromino.cs
+++ Assets/Assets/Scripts/Tetromino.cs
@@ -2,13 +2,7 @@ using UnityEngine.Tilemaps;
 using UnityEngine;
 public enum Tetromino
 {
-    I,
-    O,
-    T,
-    J,
-    L,
-    S,
-    Z,
+    I, J, L, O, S, T, Z
 }
 
 [System.Serializable]
@@ -17,9 +11,11 @@ public struct TetrominoData
     public Tetromino tetromino;
     public Tile tiles;
     public Vector2Int[] cells { get; private set; }
+    public Vector2Int[,] WallKicks { get; private set; }
 
     public void Initialize()
     {
         this.cells = Data.Cells[this.tetromino];
+        this.WallKicks = Data.WallKicks[this.tetromino];
     }
 }
